bd44a48e5445f89f1ebb78ad4f5e66d0ae3f15cb
Gabriel Reis <gabrielreistldev@gmail.com>
2025-07-19 15:05:56 -0300

    mudança na font

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -3,6 +3,6 @@ import papaBG from '../public/background-papa.png';
 
 export default function BackgroundImage() {
   return (
-    <Image src={papaBG} alt="Papa" className="absolute inset-0 bg-cover bg-center opacity-30 h-screen" />
+    <Image src={papaBG} alt="Papa" className="absolute inset-0 bg-cover bg-center opacity-40 h-screen" />
   );
 } 
